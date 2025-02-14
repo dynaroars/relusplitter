3e0fe4b52d0ff591bedac0fdb6bce536cf1f2a0d
lli <lmax.qaz@gmail.com>
2025-02-14 11:27:39 -0500

    y

--- experiment/test.py
+++
@@ -1,8 +0,0 @@
-from helpers import *
-from settings import *
-
-# def check_runned(log_fname, benchmark_instances):
-
-
-remaining = get_remaining("/home/lli/tools/relusplitter/experiment/results/exp4/mnist_fc~nnenum~stable~random~2~lambda.csv",
-             get_instances(benchmarks["mnist_fc"]))

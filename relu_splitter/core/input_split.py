8cb4c24be15efbdefacbff2e1318205ea2320ab5
lli <lmax.qaz@gmail.com>
2025-02-18 17:13:38 -0500

    clean up

---
+++ relu_splitter/core/input_split.py
@@ -0,0 +1,14 @@
+from .common import *
+
+class RSplitter_input():
+    def input_split_inplace():
+        # fc
+        # conv
+        # or even other layers
+        pass
+    
+    def input_split_w_layer():
+        # easier to implement
+        # use fc or conv depending on the input type
+        pass
+

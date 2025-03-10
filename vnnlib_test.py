ec31ff4cbfcdf234c8b780d73ea7f4fb7defe36d
lli <lmax.qaz@gmail.com>
2025-03-10 16:29:49 -0400

    vnnlib

--- vnnlib_test.py
+++ vnnlib_test.py
@@ -1,4 +1,4 @@
-from relu_splitter.utils.read_vnnlib import *
+from relu_splitter.spec import *
 
 
 fp = "/home/lli/tools/relusplitter/data/acasxu_converted/vnnlib/prop_7.vnnlib"
@@ -10,7 +10,13 @@ loaded_vnnlib = read_vnnlib(fp)
 #     2. specification, provided as a list of pairs (mat, rhs), as in: mat * y <= rhs, where y is the output.
 #                       Each element in the list is a term in a disjunction for the specification.
 
-# print(loaded_vnnlib[0][1][0])
-# print(loaded_vnnlib[0][1][1])
+test = warpped_vnnlib(loaded_vnnlib[0])
 
-print(loaded_vnnlib)
+print(test.serialize_postCondition())
+
+
+# ; unsafe if strong left is minimial or strong right is minimal
+# (assert (or
+#     (and (<= Y_3 Y_0) (<= Y_3 Y_1) (<= Y_3 Y_2))
+#     (and (<= Y_4 Y_0) (<= Y_4 Y_1) (<= Y_4 Y_2))
+# ))
